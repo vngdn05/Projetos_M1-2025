94a279fd2fb909479f6065195d63926be2042a5c
vngdn05 <viniciuslucasciardi@hotmail.com>
2025-02-10 11:58:46 -0300

    Create Atividade Ponderada (Contador de notas ecolares).js

---
+++ Atividade Ponderada (Contador de notas ecolares).js
@@ -0,0 +1,24 @@
+var m 
+
+nota1= 2
+nota2= 2
+nota3= 2
+console.log("nota 1:", nota1)
+console.log("nota 2:", nota2)
+console.log("nota 3:", nota3)
+
+m = (nota1 + nota2 +nota3)/3 
+ console.log("média:", m );
+
+ switch (true){
+
+    case m >= 7:
+    console.log("APROVADO");
+    break;
+
+    case m < 7:
+    console.log("REPROVADO");
+    break;
+ }
+
+ 
